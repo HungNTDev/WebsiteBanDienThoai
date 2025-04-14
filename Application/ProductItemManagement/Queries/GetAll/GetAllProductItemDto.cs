071650e559f1cdbf30fe99263632a491ce63b443
HungNTDev <kmac08552@gmail.com>
2025-04-14 20:01:59 +0700

    Sửa tên

--- Application/ProductItemManagement/Queries/GetAll/GetAllProductItemDto.cs
+++ Application/ProductItemManagement/Queries/GetAll/GetAllProductItemDto.cs
@@ -3,7 +3,7 @@
     public class GetAllProductItemDto
     {
         public string Name { get; set; }
-        public string ImageUrl { get; set; }
+        public string Image { get; set; }
         public decimal Price { get; set; }
         public DateTime CreateDate { get; set; }
         public string CreatedBy { get; set; }
